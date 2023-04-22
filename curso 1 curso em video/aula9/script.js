0d5c147ee746fb76bdbfadbccd58d0c3582ba747
williamcardeal <128101285+williamcardeal@users.noreply.github.com>
2023-04-22 19:56:17 -0300

    js

--- curso 1 curso em video/aula9/script.js
+++ curso 1 curso em video/aula9/script.js
@@ -1,4 +1,54 @@
+function verif(){
 
+    let data = new Date()
+
+    let ano = data.getFullYear()
+
+    let fAno = document.querySelector("#txtano")
+
+    let res = document.querySelector("#res")
+
+    if(fAno.value.length == 0 || Number(fAno.value) > ano ){
+        alert("[ERRO] Verifique os dados e tente novamente")
+    }else {
+       let sex = document.getElementsByName("radsex")
+
+       let idade = ano - Number(fAno.value)
+
+       let genero = ""
+
+       let img = document.createElement("img")
+       img.setAttribute("id, foto")
+       
+       if (sex [0].checked){
+        genero = "Masculino"
+        if(idade >= 0 && idade < 10){
+            //criança
+        } else if (idade >= 10 && idade < 21){
+            //jovem
+        } else if (idade < 21){
+            //adulto
+        }else if(idade < 50){
+            //idoso
+        }
+
+    }else if (sex [1].checked){
+          genero = "Feminono"
+       }
+       if(idade >= 0 && idade < 10){
+        //criança
+    } else if (idade >= 10 && idade < 21){
+        //jovem
+    } else if (idade < 21){
+        //adulto
+    }else if(idade < 50){
+        //idoso(a)
+    }
+       res.style.textAlign = "center"
+       res.innerHTML = `${genero} ${idade}`
+    }
+
+}
 
 
 
